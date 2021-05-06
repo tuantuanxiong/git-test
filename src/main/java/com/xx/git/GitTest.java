6f3b84d045772568b02580bf5bd30e93abe4005f
Pwq <panwenqiangwy@163.com>
2021-05-06 11:48:01 +0800

    first commit2

--- src/main/java/com/xx/git/GitTest.java
+++ src/main/java/com/xx/git/GitTest.java
@@ -5,6 +5,7 @@ package com.xx.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.printf("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
